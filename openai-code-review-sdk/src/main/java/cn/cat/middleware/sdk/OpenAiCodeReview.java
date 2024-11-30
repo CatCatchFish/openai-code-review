382142eefd16949835eadade930d92a7467534dd
CatFish <3285109052@qq.com>
2024-11-30 11:36:40 +0800

    ChatGLM代码评审

--- openai-code-review-sdk/src/main/java/cn/cat/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/cat/middleware/sdk/OpenAiCodeReview.java
@@ -1,8 +1,16 @@
 package cn.cat.middleware.sdk;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
+import cn.cat.middleware.sdk.domain.model.ChatCompletionRequest;
+import cn.cat.middleware.sdk.domain.model.ChatCompletionSyncResponse;
+import cn.cat.middleware.sdk.domain.model.Model;
+import cn.cat.middleware.sdk.types.utils.BearerTokenUtils;
+import com.alibaba.fastjson2.JSON;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class OpenAiCodeReview {
 
@@ -26,7 +34,69 @@ public class OpenAiCodeReview {
         int exitCode = process.waitFor();
         System.out.println("Exited with code:" + exitCode);
 
-        System.out.println("评审代码：" + diffCode);
+        // 2. chatglm 代码评审
+        String log = codeReview(diffCode.toString());
+        System.out.println("code review：" + log);
+
     }
 
+    private static String codeReview(String diffCode) throws Exception {
+        String apiKeySecret = "2da3e6fab628094ec02e201d657413d8.e8vqnK8CNpbuXbQM";
+        String token = BearerTokenUtils.getToken(apiKeySecret);
+
+        HttpURLConnection connection = getHttpURLConnection(token);
+
+        ChatCompletionRequest chatCompletionRequest = getChatCompletionRequest(diffCode);
+
+        try (OutputStream os = connection.getOutputStream()) {
+            byte[] input = JSON.toJSONString(chatCompletionRequest).getBytes(StandardCharsets.UTF_8);
+            os.write(input);
+        }
+
+        int responseCode = connection.getResponseCode();
+        System.out.println(responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+
+        in.close();
+        connection.disconnect();
+
+        System.out.println("评审结果：" + content);
+
+        ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
+        return response.getChoices().get(0).getMessage().getContent();
+
+    }
+
+    private static ChatCompletionRequest getChatCompletionRequest(String diffCode) {
+        ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest();
+        chatCompletionRequest.setModel(Model.GLM_4_PLUS.getCode());
+        chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>() {
+            private static final long serialVersionUID = -7988151926241837899L;
+
+            {
+                add(new ChatCompletionRequest.Prompt("user", "你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码如下:"));
+                add(new ChatCompletionRequest.Prompt("user", diffCode));
+            }
+        });
+        return chatCompletionRequest;
+    }
+
+    private static HttpURLConnection getHttpURLConnection(String token) throws IOException {
+        URL url = new URL("https://open.bigmodel.cn/api/paas/v4/chat/completions");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Authorization", "Bearer " + token);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+        connection.setDoOutput(true);
+        return connection;
+    }
 }
--- openai-code-review-sdk/src/test/java/cn/cat/middleware/sdk/test/ApiTest.java
+++ openai-code-review-sdk/src/test/java/cn/cat/middleware/sdk/test/ApiTest.java
@@ -1,19 +1,76 @@
 package cn.cat.middleware.sdk.test;
 
-import lombok.extern.slf4j.Slf4j;
+import cn.cat.middleware.sdk.domain.model.ChatCompletionSyncResponse;
+import cn.cat.middleware.sdk.types.utils.BearerTokenUtils;
+import com.alibaba.fastjson2.JSON;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@Slf4j
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
 public class ApiTest {
 
     @Test
-    public void test() {
-        System.out.println("test");
+    public void test_http() throws IOException {
+        String apiKeySecret = "2da3e6fab628094ec02e201d657413d8.e8vqnK8CNpbuXbQM";
+        String token = BearerTokenUtils.getToken(apiKeySecret);
+
+        HttpURLConnection connection = getHttpURLConnection(token);
+
+        int responseCode = connection.getResponseCode();
+        System.out.println(responseCode);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null){
+            content.append(inputLine);
+        }
+
+        in.close();
+        connection.disconnect();
+
+        ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
+        System.out.println(response.getChoices().get(0).getMessage().getContent());
+
     }
 
+    private static HttpURLConnection getHttpURLConnection(String token) throws IOException {
+        HttpURLConnection connection = getUrlConnection(token);
+
+        String code = "1+1";
+        String jsonInpuString = "{"
+                + "\"model\":\"glm-4-flash\","
+                + "\"messages\": ["
+                + "    {"
+                + "        \"role\": \"user\","
+                + "        \"content\": \"你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码为: " + code + "\""
+                + "    }"
+                + "]"
+                + "}";
+
+        try(OutputStream os = connection.getOutputStream()){
+            byte[] input = jsonInpuString.getBytes(StandardCharsets.UTF_8);
+            os.write(input);
+        }
+        return connection;
+    }
+
+    private static HttpURLConnection getUrlConnection(String token) throws IOException {
+        URL url = new URL("https://open.bigmodel.cn/api/paas/v4/chat/completions");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Authorization", "Bearer " + token);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+        connection.setDoOutput(true);
+        return connection;
+    }
 }
