05ed00819feb602aae92e56d0d99d1771491e6de
CatFish <3285109052@qq.com>
2025-02-26 22:09:00 +0800

    版本 1.2

---
+++ openai-code-review-sdk/src/test/java/cn/cat/middleware/sdk/test/model/WorkflowTest.java
@@ -0,0 +1,13 @@
+package cn.cat.middleware.sdk.test.model;
+
+import org.junit.Test;
+
+public class WorkflowTest {
+
+    @Test
+    public void test() {
+        // 测试 workflow 流程
+        System.out.println("workflow 测试流程");
+    }
+
+}
